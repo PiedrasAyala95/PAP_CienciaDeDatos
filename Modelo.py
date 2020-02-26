0f7cc50bda0af1e3278f9a62cd90722284bca99a
PiedrasAyala95 <if700049@iteso.mx>
2020-02-26 11:58:10 -0600

    Avances

--- Modelo.py
+++ Modelo.py
@@ -8,13 +8,33 @@ Created on Sat Feb 22 10:28:44 2020
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
-from Mylib import mylib
+import Mylib as mylib
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import (accuracy_score,precision_score,recall_score)
 
-#%%------------------------------------Modelo 3 --------------------------------------
+#%%
 
-#%% importacion de los datos a analizar 
-#data = pd.read_csv('C:/Users/Piedras-Private/Desktop/5 Semestre/Ciencia de datos/Datos/HR_comma_sep.csv',header = None)
-#data.columns = ['Satisfaction','Last Evaluation','Number Project','Average montly','Time spend','Work accident','Left','Promotion 5 years','Sales','Salary']
+Datos_Ameca = pd.read_csv('C:/Users/Piedras/Documents/Github/PAP_CienciaDeDatos/Datos_Clima/Ameca.csv',header=0)
+Reporte_Ameca = mylib.mylib.dqr(Datos_Ameca)
+
+Col_maxtemp= Datos_Ameca.iloc[:,1] 
+
+#%%
+
+datos_fechas= np.arange(np.datetime64('2009-01-01'), np.datetime64('2020-02-21'))
+
+i = 0
+Max_temp =np.array(np.zeros(4068))
+for j in range(4068):
+    Max_temp[j] = Col_maxtemp[i]
+    i=i+24
+    
+ #%%
+plt.figure(figsize=(15,10))
+plt.plot(datos_fechas,Max_temp,linewidth=0.7)
+plt.title('Movimiento de la Temperatura Maxima')
+plt.xlabel('Años')
+plt.ylabel('Temperatura')
+plt.grid(True)
+plt.show()   
